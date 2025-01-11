d3e2936b4f3bc8c1d8360fd14e224cd38a2e9399
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2025-01-10 23:39:17 -0600

    Update pokedex.c

--- src/pokedex.c
+++ src/pokedex.c
@@ -4661,7 +4661,7 @@ bool16 HasSeenAllHoennMons(void)
 {
     u32 i, j;
 
-    for (i = ; i < HOENN_DEX_COUNT; i++)
+    for (i = 0; i < HOENN_DEX_COUNT; i++)
     {
         j = HoennToNationalOrder(i + 1);
         if (!(gSpeciesInfo[j].isMythical && !gSpeciesInfo[j].dexForceRequired) && !GetSetPokedexFlag(j, FLAG_GET_SEEN))
